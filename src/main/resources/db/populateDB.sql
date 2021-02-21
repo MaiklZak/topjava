06b575edc3050ff310b39025b313f52bd18a34af
maiklzak <maiklzak@gmail.com>
2021-02-21 16:52:05 +0300

    create and init tab meals, constructor def

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+
+INSERT INTO meals (description, calories, datetime, user_id)
+VALUES ('Breakfast', 500, '2020-01-30 10:00:00', 100000),
+       ('Lunch', 1000, '2020-01-30 13:00:00', 100000 ),
+       ('Dinner', 850, '2020-01-30 19:00:00', 100000),
+       ('Breakfast', 700, '2020-01-31 10:30:00', 100000),
+       ('Lunch', 1100, '2020-01-31 13:30:00', 100001),
+       ('Dinner', 650, '2020-01-31 19:00:00', 100001);

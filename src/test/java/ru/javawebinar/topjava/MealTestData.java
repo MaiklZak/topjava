70595aaf80ea1c2067513c888d64991afad513f1
maiklzak <maiklzak@gmail.com>
2021-02-22 17:31:29 +0300

    create MealServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MealTestData {
+
+    public static final Meal meal = new Meal(100002, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Breakfast", 500);
+    public static final Meal meal1 = new Meal(100003, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Lunch", 1000);
+    public static final Meal meal2 = new Meal(100004, LocalDateTime.of(2020, Month.JANUARY, 30, 19, 0), "Dinner", 850);
+    public static final Meal meal3 = new Meal(100005, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 30), "Breakfast", 700);
+    public static final Meal meal4 = new Meal(100006, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 30), "Lunch", 1100);
+    public static final Meal meal5 = new Meal(100007, LocalDateTime.of(2020, Month.JANUARY, 31, 19, 0), "Dinner", 650);
+
+    public static final Meal mealUpdate = new Meal(100002, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "UpdateBreak", 3000);
+
+    public static final Meal mealNew = new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 14, 0), "NewLunch", 2000);
+
+    public static final List<Meal> listMeals = new ArrayList<>();
+    public static final List<Meal> listMealsBetween = new ArrayList<>();
+    public static final List<Meal> listMealsAll = new ArrayList<>();
+
+
+    static {
+        listMeals.add(meal);
+        listMeals.add(meal1);
+        listMeals.add(meal2);
+        listMeals.add(meal3);
+        listMeals.add(meal4);
+        listMeals.add(meal5);
+
+        listMealsAll.add(meal);
+        listMealsAll.add(meal1);
+        listMealsAll.add(meal2);
+        listMealsAll.add(meal3);
+        listMealsAll.sort((m1, m2) -> m2.getDateTime().compareTo(m1.getDateTime()));
+
+
+        listMealsBetween.add(meal);
+        listMealsBetween.add(meal1);
+        listMealsBetween.add(meal2);
+        listMealsBetween.sort((m1, m2) -> m2.getDateTime().compareTo(m1.getDateTime()));
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,95 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal mealGet = service.get(meal.getId(), UserTestData.USER_ID);
+        assertThat(mealGet).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(meal);
+    }
+
+    @Test
+    public void delete() {
+        service.delete(meal.getId(), UserTestData.USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(meal.getId(), UserTestData.USER_ID));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        List<Meal> betweenInclusive = service.getBetweenInclusive(LocalDate.of(2020, Month.JANUARY, 30),
+                LocalDate.of(2020, Month.JANUARY, 30), UserTestData.USER_ID);
+        assertThat(betweenInclusive).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(listMealsBetween);
+    }
+
+    @Test
+    public void getAll() {
+        List<Meal> actualList = service.getAll(UserTestData.USER_ID);
+        assertThat(actualList).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(listMealsAll);
+    }
+
+    @Test
+    public void update() {
+        service.update(mealUpdate, UserTestData.USER_ID);
+        assertThat(mealUpdate).usingRecursiveComparison().ignoringFields("dateTime")
+                .isEqualTo(service.get(mealUpdate.getId(), UserTestData.USER_ID));
+
+    }
+
+    @Test
+    public void create() {
+        Meal mealCreated = service.create(mealNew, UserTestData.USER_ID);
+        mealNew.setId(mealCreated.getId());
+        assertThat(mealNew).usingRecursiveComparison().ignoringFields("dateTime")
+                .isEqualTo(service.get(mealNew.getId(), UserTestData.USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(meal.getId(), UserTestData.NOT_FOUND));
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(meal.getId(), UserTestData.NOT_FOUND));
+    }
+
+    @Test
+    public void duplicateDateTimeCreate() {
+        assertThrows(DataAccessException.class, () -> service.update(mealUpdate, UserTestData.NOT_FOUND));
+    }
+}
